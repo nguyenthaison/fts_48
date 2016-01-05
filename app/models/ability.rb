4a9bf84d79d10cc449c6041db15cf0593e12057c
SonNT91 <nguyenthaison9193@gmail.com>
2016-01-05 13:20:17 +0700

    authorization cancancan gem

---
+++ app/models/ability.rb
@@ -0,0 +1,17 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize user
+    alias_action :create, :show, :update, :new, :index, to: :crud
+    return if user.nil?
+    if user.admin?
+      can :manage, :all
+    else
+      can [:update, :show], User, id: user.id
+      can [:index], Subject
+      can [:new, :create, :index], Question
+      can [:edit, :update, :destroy], Question, user_id: user.id
+      can [:crud], Exam, user_id: user.id
+    end
+  end
+end
