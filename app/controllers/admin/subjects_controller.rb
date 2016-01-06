514bff2c846640f481a64d9dd7bba83dcf9bbe2c
hiennv94 <hiennv.th@gmail.com>
2016-01-06 16:36:37 +0700

    Finish task create and view subject

---
+++ app/controllers/admin/subjects_controller.rb
@@ -0,0 +1,41 @@
+class Admin::SubjectsController < ApplicationController
+  load_and_authorize_resource
+
+  def index
+    @subjects = @subjects.page params[:page]
+  end
+
+  def update
+    if @subject.update_attributes subject_params
+      flash[:success] = t "admin.subject.flash_update_success"
+      redirect_to admin_subjects_path
+    else
+      flash[:danger] = t "admin.subject.flash.flash_update_fail"
+      render :edit
+    end
+  end
+
+  def create
+    if @subject.save
+      flash[:success] = t "admin.subject.new_subject_success"
+      redirect_to admin_subjects_path
+    else
+      flash[:danger] = t "admin.subject.new_subject_fail"
+      render :new
+    end
+  end
+
+  def destroy
+    if @subject.destroy
+      flash[:success] = t "admin.subject.destroy_success"
+    else
+      flash[:danger] = t "admin.subject.destroy_fail"
+    end
+    redirect_to admin_subjects_path
+  end
+
+  private
+  def subject_params
+    params.require(:subject).permit :name
+  end
+end
