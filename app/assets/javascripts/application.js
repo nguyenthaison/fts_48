80ebf66757468a3cad91d6fe94cab19021f172a6
SonNT91 <nguyenthaison9193@gmail.com>
2016-01-08 11:45:44 +0700

    start exam

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,3 +26,38 @@ function remove_fields(link) {
    var regexp = new RegExp("new_" + association, "g")
    $(link).parent().before(content.replace(regexp, new_id));
  }
+
+function cowntdown(){
+  now = new Date();
+  diff = end - now;
+  diff = new Date(diff);
+  var sec = diff.getSeconds();
+  var min = diff.getMinutes();
+  var hours = diff.getHours();
+  if (min < 10){
+    min = "0" + min;
+  }
+  if (sec < 10){
+    sec = "0" + sec;
+  }
+  hours = "00";
+  if(now >= end){
+    clearTimeout(timerID);
+    alert($("#cdtime").data("msgout"));
+  }
+  else{
+    document.getElementById("cdtime").innerHTML = hours + ":" + min + ":" + sec;
+    timerID = setTimeout("cowntdown()", 1000);
+  }
+}
+var ready = function() {
+  var minutesleft = 0;
+  var secondsleft =  parseInt($("#cdtime").data("duration")*60);
+  end = new Date();
+  end.setMinutes(end.getMinutes()+minutesleft);
+  end.setSeconds(end.getSeconds()+secondsleft);
+  cowntdown();
+};
+
+$(document).ready(ready);
+$(document).on("page:load", ready);
--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -16,9 +16,13 @@ class ExamsController < ApplicationController
     end
   end
 
+  def show
+    @exam.change_status_after_start
+  end
+
   private
   def exam_params
     params.require(:exam).permit :duration, :number_of_question,
-      :status, :user_id, :subject_id
+      :status, :user_id, :subject_id, results_attributes: [:id, content_answer: []]
   end
 end
